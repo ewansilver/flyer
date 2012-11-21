0da50cc467359f47b8d38148b08be16cb08df7ec
ewan <ewan@ewansilver.com>
2012-11-21 13:25:16 +0000

    Create a very basic, unnamed gen_server without any supervisor support.

--- src/fly.erl
+++ src/fly.erl
@@ -17,286 +17,148 @@
 %% under the License.
 %%
 %% -------------------------------------------------------------------
-
+%% Description : Gen Server implementation for a Fly server.
 %% Author: Ewan Silver
-%% Created: 27 Apr 2010
-
+%% Created : 20 Nov 2012
+%%% -------------------------------------------------------------------
 -module(fly).
 
-%%
+-behaviour(gen_server).
+%% --------------------------------------------------------------------
 %% Include files
-%%
--record(state,
-			{socket,
-	 		type_structures=[]}).
-%%
-%% Exported Functions
-%%
--export([connect/0, connect/2,close/1,
-		 ping/1,
-		 read/3,
-		 read/4,
-		 write/4,
-		 take/3,
-		 take/4,
-		 read_many/4,
-		 take_many/4,
-		 register_entry/2,
-		 register_entry/3]).
+%% --------------------------------------------------------------------
 
-%%-export([binary_read/0]).
-%%
-%% API Functions
-%%
--define(ZnInt, 32/big).
--define(ZnLong, 64/big).
--define(ZnUUID, 128/big).
+%% --------------------------------------------------------------------
+%% External exports
+-export([
+		 start/0,
+		 stop/0,
+		 ping/0,
+		 read/2,
+		 take/2,
+		 write/3,
+%%		 take/3,
+%%		 take/4,
+%%		 read_many/4,
+%%		 take_many/4,
+%%		 register_entry/2,
+		 register/2]).
 
-connect() ->
-	connect("localhost",4396).
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
-connect(Host,Port) ->
-    {ok, Sock} = gen_tcp:connect(Host,Port, 
-                                 [binary, 
-								  {active, false} ]),
-	#state{socket=Sock}.
+-record(state, {state}).
 
-close(State) ->
-	gen_tcp:close(State#state.socket).
+%% ====================================================================
+%% External functions
+%% ====================================================================
+
+start() ->
+	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+stop() ->
+	gen_server:call(?MODULE,{stop}).
+
+ping() ->
+	gen_server:call(?MODULE, {ping}).
+
+register(Typename,Field_info) ->
+	gen_server:call(?MODULE,{register,Typename,Field_info}).
+
+read(Typename, Template) ->
+	gen_server:call(?MODULE,{read,Typename,Template}).
+
+take(Typename, Template) ->
+	gen_server:call(?MODULE,{take,Typename,Template}).
+
+write(Typename, Template, Lease) ->
+	gen_server:call(?MODULE,{write,Typename,Template,Lease}).
 	
-%% Return: {ping,[tagnames]}
-ping(State) ->
-	Header = <<16#FAB20000:?ZnInt>>,
-    ok = gen_tcp:send(State#state.socket, Header),
-	{ok,<<_TagCount:?ZnLong>>} = gen_tcp:recv(State#state.socket,8),
-	{ok,Response} = gen_tcp:recv(State#state.socket,0),
-	{ping, get_tags(Response,[])}.
+%% ====================================================================
+%% Server functions
+%% ====================================================================
 
-%% Return: {read,number of matches: 1 or 0,Entry or any empty tuple}
-read(_State,_Type_name, _Template, {Time_left,_Repeat_time}) when Time_left < 0 ->
-	{read,0,{}};
-read(State,Type_name, Template, {Time_left,Repeat_time}) ->
-	case read(State,Type_name, Template) of
-		{read,0,{}} -> pause(Repeat_time),
-					   read(State,Type_name, Template,{Time_left-Repeat_time,Repeat_time});
-		Else-> Else
-	end;
-read(State,Type_name, Template, Time_left) ->
-	read(State,Type_name, Template, {Time_left,Time_left}).
-					   
-read(State,Type_name, Template) ->
-	case lists:keysearch(Type_name, 1,State#state.type_structures) of
-		false -> {fail, missing_type};
-		{value,{_,Type_channel,_,Field_info}} ->
-			Template_entry = create_Zn_entry(Template),
-			Header = <<16#FAB20001:?ZnInt>>,
-			Binary = <<Header/binary,Type_channel:?ZnInt, Template_entry/binary,0:?ZnLong>>,
-			parse_single_entry_responses(read,State#state.socket,Field_info,Binary)
-	end.
+%% --------------------------------------------------------------------
+%% Function: init/1
+%% Description: Initiates the server
+%% Returns: {ok, State}          |
+%%          {ok, State, Timeout} |
+%%          ignore               |
+%%          {stop, Reason}
+%% --------------------------------------------------------------------
+init([]) ->
+	State = fly_protocol:connect(),
+    {ok, #state{state=State}}.
 
-%% Return: {take,number of matches: 1 or 0,Entry or any empty tuple}
-take(_State,_Type_name, _Template, {Time_left,_Repeat_time}) when Time_left < 0 ->
-	{take,0,{}};
-take(State,Type_name, Template, {Time_left,Repeat_time}) ->
-	case take(State,Type_name, Template) of
-		{take,0,{}} -> pause(Repeat_time),
-					   take(State,Type_name, Template,{Time_left-Repeat_time,Repeat_time});
-		Else-> Else
-	end;
-take(State,Type_name, Template, Time_left) ->
-	take(State,Type_name, Template, {Time_left,Time_left}).
+%% --------------------------------------------------------------------
+%% Function: handle_call/3
+%% Description: Handling call messages
+%% Returns: {reply, Reply, State}          |
+%%          {reply, Reply, State, Timeout} |
+%%          {noreply, State}               |
+%%          {noreply, State, Timeout}      |
+%%          {stop, Reason, Reply, State}   | (terminate/2 is called)
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+handle_call({ping}, _From, State) ->
+	{ping,Tags} = fly_protocol:ping(State#state.state),
+    Reply = Tags,
+    {reply, Reply, State};
+handle_call({register,Typename,Field_info}, _rom, State) ->
+	{ok,S} = fly_protocol:register_entry(State#state.state,{Typename,Field_info}),
+    Reply = ok,
+	{reply, Reply, State#state{state=S}};
+handle_call({read,Typename,Template}, _From, State) ->
+	Reply = fly_protocol:read(State#state.state,Typename,Template),
+    {reply, Reply, State};
+handle_call({take,Typename,Template}, _From, State) ->
+	Reply = fly_protocol:take(State#state.state,Typename,Template),
+    {reply, Reply, State};
+handle_call({write,Typename,Template,Lease}, _From, State) ->
+	{write,L} = fly_protocol:write(State#state.state,Typename,Template,Lease),
+	Reply = {ok,L},
+    {reply, Reply, State};
+handle_call({stop}, _From, State) ->
+	Reply = fly_protocol:close(State#state.state),
+	{stop, normal, shutdown_ok, State}.
 
-take(State,Type_name, Template) ->
-	case lists:keysearch(Type_name, 1,State#state.type_structures) of
-		false -> {fail, missing_type};
-		{value,{_,Type_channel,_,Field_info}} ->
-			Template_entry = create_Zn_entry(Template),
-			Header = <<16#FAB20002:?ZnInt>>,
-			Binary = <<Header/binary,Type_channel:?ZnInt, Template_entry/binary,0:?ZnLong>>,
-			parse_single_entry_responses(take,State#state.socket,Field_info, Binary)
-	end.
+%% --------------------------------------------------------------------
+%% Function: handle_cast/2
+%% Description: Handling cast messages
+%% Returns: {noreply, State}          |
+%%          {noreply, State, Timeout} |
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+handle_cast(Msg, State) ->
+    {noreply, State}.
 
-parse_single_entry_responses(Type,Sock,Field_info,Request_binary) ->
-	ok = gen_tcp:send(Sock, Request_binary),
-	{ok,<<Num_fields:?ZnLong>>} = gen_tcp:recv(Sock,8),
-	
-	case Num_fields of
-		0 -> {Type,0, {}};
-		_ -> {ok,<<_UUID:?ZnUUID,Binary/binary>>} = gen_tcp:recv(Sock,0),
-			{Entry,_} = get_object(Num_fields,Binary,Field_info),
-			{Type, 1,Entry}
-	end.
+%% --------------------------------------------------------------------
+%% Function: handle_info/2
+%% Description: Handling all non call/cast messages
+%% Returns: {noreply, State}          |
+%%          {noreply, State, Timeout} |
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+handle_info(Info, State) ->
+    {noreply, State}.
 
-pause(Wait) ->
-	timer:sleep(Wait).
+%% --------------------------------------------------------------------
+%% Function: terminate/2
+%% Description: Shutdown the server
+%% Returns: any (ignored by gen_server)
+%% --------------------------------------------------------------------
+terminate(Reason, State) ->
+    ok.
 
-%% Return: {write,Lease time in ms}
-write(State,Type_name, Template, Lease) ->
-	case lists:keysearch(Type_name, 1,State#state.type_structures) of
-		false -> {fail, missing_type};
-		{value,{_,Type_channel,_,_}} ->
-			ID_entry = create_id_object(create_Zn_entry(Template)),
-			Header = <<16#FAB20003:?ZnInt>>,
-			Timeout = 1000,
-		    ok = gen_tcp:send(State#state.socket, <<Header/binary,Type_channel:?ZnInt, ID_entry/binary,Lease:?ZnLong>>),
-%%io:format("~p ~p\n",["Written",ID_entry]),
-			{ok,<<Lease_granted:?ZnLong>>} = gen_tcp:recv(State#state.socket,0,Timeout),
-			{write,Lease_granted}
-	end.
+%% --------------------------------------------------------------------
+%% Func: code_change/3
+%% Purpose: Convert process state when code is changed
+%% Returns: {ok, NewState}
+%% --------------------------------------------------------------------
+code_change(OldVsn, State, Extra) ->
+    {ok, State}.
 
-%% Return: {read_many,number of matches,list of entries - could be empty}
-read_many(State,Type_name, Template, Limit) ->
-	case lists:keysearch(Type_name, 1,State#state.type_structures) of
-		false -> {fail, missing_type};
-		{value,{_,Type_channel,_,Field_info}} ->
-			Template_entry = create_Zn_entry(Template),
-			Header = <<16#FAB20006:?ZnInt>>,
-%% Question: what is ignore for?
-			Ignore = 0,
-		    ok = gen_tcp:send(State#state.socket,
-						<<Header/binary,Type_channel:?ZnInt, Template_entry/binary,Limit:?ZnLong,Ignore:?ZnLong>>),
-			parse_multiple_entry_responses(read_many,State#state.socket,Field_info)
-	end.
+%% --------------------------------------------------------------------
+%%% Internal functions
+%% --------------------------------------------------------------------
 
-%% Return: {take_many,number of matches,list of entries - could be empty}
-take_many(State,Type_name, Template, Limit) ->
-	case lists:keysearch(Type_name, 1,State#state.type_structures) of
-		false -> {fail, missing_type};
-		{value,{_,Type_channel,_,Field_info}} ->
-			Template_entry = create_Zn_entry(Template),
-			Header = <<16#FAB20007:?ZnInt>>,
-		    ok = gen_tcp:send(State#state.socket, 
-						<<Header/binary,Type_channel:?ZnInt, Template_entry/binary,Limit:?ZnLong>>),
-			parse_multiple_entry_responses(take_many,State#state.socket,Field_info)
-	end.
-
-parse_multiple_entry_responses(Type,Sock,FieldTypes) ->
-	{ok,<<Num_entries:?ZnLong>>} = gen_tcp:recv(Sock,8),
-	case Num_entries of
-		0 -> {Type,0, []};
-		_ -> {ok,Binary} = gen_tcp:recv(Sock,0),
-			{Entries,_} = get_entries(Num_entries,Binary,[],FieldTypes),
-			{Type, Num_entries,Entries}
-	end.
-
-register_entry(State,{Typename,FieldInfo}) ->
-	register_entry(State,Typename,[{Field,"name"} || Field <- tuple_to_list(FieldInfo)]).
-
-register_entry(State,Typename,Field_info) ->
-	FieldInfo = [{atom_to_list(Field),Name} || {Field,Name} <- Field_info],
-	Entry_layout = create_entry_layout(Typename, FieldInfo),
-	Header = <<16#FAB2000A:?ZnInt>>,
-    ok = gen_tcp:send(State#state.socket, <<Header/binary,Entry_layout/binary>>),
-	{ok,<<Reply_code:?ZnLong>>} = gen_tcp:recv(State#state.socket,8),
-	case Reply_code of
-		0 ->
-			{ok,Entry_layout_rtn} = gen_tcp:recv(State#state.socket,0),
-			{_,TypeChannel,_,_} = decode_entry_layout(Entry_layout_rtn),
-			{ok,State#state{type_structures = [{Typename,TypeChannel,null,FieldInfo}|State#state.type_structures]}};
-		_ -> {fail, existing_type}
-	end.
-
-%%
-%% Local Functions
-%%
-
--spec create_Zn_string(atom()) -> binary().
-create_Zn_string(String) ->
-	Binary = term_to_binary(String),
-	Length = size(Binary),
-	<<Length:?ZnInt,Binary/binary>>.
-
-%% FieldInfo [{fieldType,fieldName}]
-create_entry_layout(Typename,FieldInfo) ->
-	Typename_bin = create_Zn_string(Typename),
-	FI_Length = length(FieldInfo),
-	FI_fun = fun({FieldType,FieldName},Binary) -> TypeString = create_Zn_string(FieldType),
-												  NameString = create_Zn_string(FieldName),
-					 								<<Binary/binary,TypeString/binary,NameString/binary>> end,
-	FI_binaries = lists:foldl(FI_fun,<<FI_Length:?ZnInt>>,FieldInfo),
-	<<Typename_bin/binary,0:?ZnInt,FI_binaries/binary>>.
-
-decode_entry_layout(<<TypeNameSize:?ZnInt,TypeName:TypeNameSize/binary,TypeChannel:?ZnInt,FieldInfoCount:?ZnInt,Fields/binary>>) ->
-	Field_list = [{FieldType,FieldName} ||<<TypeSize:?ZnInt,FieldType:TypeSize/binary,NameSize:?ZnInt,FieldName:NameSize/binary>> <= Fields],
-	{binary_to_list(TypeName),TypeChannel,FieldInfoCount,Field_list}.
-
-get_tags(<<>>,List) ->
-	lists:reverse(List);
-get_tags(Binary,List) ->
-	{_, String,Remainder}= get_Zn_string(Binary),
-	get_tags(Remainder,[String|List]).
-
-get_Zn_string(<<Size:?ZnInt,String:Size/binary,Remainder/binary>>) ->
-	{Size,binary_to_list(String),Remainder}.
-
-encode_Zn_object(Object) when is_binary(Object) -> 
-	create_binary_object(Object);
-encode_Zn_object(Object) when is_tuple(Object) ->
-	create_binary_object(term_to_binary(Object));
-encode_Zn_object(Object) when is_list(Object) ->
-	create_binary_object(list_to_binary(Object));
-encode_Zn_object(Object) when is_integer(Object) ->
-		create_binary_object(term_to_binary(Object));
-encode_Zn_object(Object) when is_atom(Object) ->
-		create_binary_object(term_to_binary(Object)).
-
-%%TODO: this is misnamed
-create_binary_object(Binary) ->
-	Length = size(Binary),
-	<<Length:?ZnLong,Binary/binary>>.
-
-decode_Zn_object(<<Size:?ZnInt,Object:Size/binary,Remainder/binary>>) ->
-	{Size,Object,Remainder}.
-
-create_id_object(<<Length:?ZnInt,Objects/binary>>) ->
-	UUID = <<16#123:?ZnUUID>>,
-	<<Length:?ZnInt,UUID/binary,Objects/binary>>.
-
-%% Objects = [Object]
-create_Zn_entry(Objects) when is_list(Objects) ->
-	Length = length(Objects),
-	lists:foldl(fun(Object,Binary) -> <<Binary/binary,Object/binary>> end, <<Length:?ZnInt>>,Objects);
-create_Zn_entry(Tuple) ->
-	create_Zn_entry(lists:map(fun(Element) -> encode_Zn_object(Element) end, tuple_to_list(Tuple))).
-
-get_Zn_entry(<<Num_of_fields:?ZnLong,UUID1:?ZnLong,UUID2:?ZnLong,Binary/binary>>,FieldTypes) ->
-	{Fields,RemainingBinary} = get_object(Num_of_fields,Binary,FieldTypes),
-	{Fields,RemainingBinary}.
-
-get_entries(0,Remaining_binary,List,_) -> 
-	{lists:reverse(List),Remaining_binary};
-get_entries(Num_entries,Entries,List,FieldTypes) ->
-	{Entry,RemainingBinary} = get_Zn_entry(Entries,FieldTypes),
-	get_entries((Num_entries-1), RemainingBinary,[Entry|List],FieldTypes).
-
-get_object(Num_fields,Binary,FieldTypes) -> 
-	%% We have to double the number of fields becuase Fly core is expecting a field name in the first
-	%% (and every other) location. To turn this into an Erlang tuple we then need to strip out those empty tuples.
-	{Fields,Remainder} = get_objects((Num_fields*2), Binary,[]),
-	{list_to_tuple(remove_field_names(Fields,[],FieldTypes)),Remainder}.
-
-remove_field_names([],Acc,_) -> 
-	lists:reverse(Acc);
-remove_field_names([_Name,Type|T],Acc,[F|FieldTypes]) -> 
-	remove_field_names(T,[decode_field_type(Type,F)|Acc],FieldTypes).
-
-%% Todo: Decode field type is the place we will work out how to deserialise things into something other than strings in the future.
-%% We should be able to return atoms and terms to!
-%% TODO: not sure why the binary is needed here. It seems to die on read_many if I don't catch an empty binary here.
-decode_field_type(<<>>,_) -> 
-	<<>>;
-decode_field_type(Field,{Type,_}) -> 
-	case Type of
-		"atom" -> binary_to_term(Field);
-		"int" -> binary_to_term(Field);
-		"tuple" -> binary_to_term(Field);
-		"binary" -> Field;
-		"string" -> binary_to_list(Field);
-		_ 	-> binary_to_list(Field)
-	end.
-
-get_objects(0,Remainder,List) ->
-	{lists:reverse(List),Remainder};
-get_objects(Num_of_fields, Objects,List) ->
-	{_Size, Object,Remainder} = decode_Zn_object(Objects),
-	get_objects((Num_of_fields-1), Remainder,[Object|List]).
--- test/fly_tests.erl
+++ test/fly_tests.erl
@@ -1,27 +1,6 @@
-%% -------------------------------------------------------------------
-%%
-%% Copyright (c) 2010 Ewan Silver.  All Rights Reserved.
-%%
-%% This file is provided to you under the Apache License,
-%% Version 2.0 (the "License"); you may not use this file
-%% except in compliance with the License.  You may obtain
-%% a copy of the License at
-%%
-%%   http://www.apache.org/licenses/LICENSE-2.0
-%%
-%% Unless required by applicable law or agreed to in writing,
-%% software distributed under the License is distributed on an
-%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-%% KIND, either express or implied.  See the License for the
-%% specific language governing permissions and limitations
-%% under the License.
-%%
-%% -------------------------------------------------------------------
-
-%% Author: Ewan Silver
-%% Created: 27 Apr 2010
-%% Description: Fly Eunit test module.
-
+%% Author: ewan
+%% Created: 20 Nov 2012
+%% Description: TODO: Add description to fly_tests
 -module(fly_tests).
 
 %%
@@ -34,197 +13,48 @@
 %%
 -export([]).
 
-%%%% 
-%% Tests
-%%%%
+%%
+%% API Functions
+%%
 
-test_connect() ->
-	fly:connect().
+ensure_can_ping_test() ->
+	{ok,_Pid} = fly:start(),
+	["FlySpace"] = fly:ping(),
+	shutdown_ok = fly:stop().
 
-%% Test
-check_can_ping_a_fly_instance_test() ->
-	State = test_connect(),
-	Tags = fly:ping(State),
-	io:fwrite("Tags: ~p~n",[Tags]),
-	fly:close(State).
+check_can_write_string_test() ->
+	Typename = check_can_write_string_test,
+	{ok,_Pid} = fly:start(),
+	ok = fly:register(Typename,{string,string}),
+	{ok,10000} = fly:write(Typename,{"name","ewan"},10000),
+	shutdown_ok = fly:stop().
 
-ensure_a_missing_type_fails_test() ->
-	State = test_connect(),
-	
-		%% Check what happens if ask for an entry type that is not present
-	{fail, missing_type} = fly:write(State, missing,{"Ewan","Silver",100, male},1000),
-	{fail, missing_type} = fly:read(State, missing,{"","",100, ""}),
-	{fail, missing_type} = fly:take(State, missing,{"","",100, ""}),
-	{fail, missing_type} = fly:read_many(State,missing, {"","",100,""},10),	
-	{fail, missing_type} = fly:take_many(State,missing, {"","",100,""},10),
-	fly:close(State).
-	
-check_can_write_read_and_take_all_formats_test() ->
-	%% Try out the alternative registration mechanism	
-	Type = check_can_write_read_and_take_all_formats_test,
-	State = test_connect(),
-	{ok,State1} = fly:register_entry(State,{Type,{string,string,int,atom}}),
+check_can_write_and_then_read_string_test() ->
+	Typename = check_can_write_and_then_read_string_test,
+	{ok,_Pid} = fly:start(),
+	ok = fly:register(Typename,{string,string}),
+	{ok,10000} = fly:write(Typename,{"name","ewan"},10000),
+	{read,1,{"name","ewan"}} = fly:read(Typename,{"name",""}),
+	shutdown_ok = fly:stop().
 
-	{write,1000} = fly:write(State1, Type,{"Ewan","Silver",100, male},1000),	
-	{write,1000} = fly:write(State1, Type,{"Alison","Silver",100, female},1000),
-	{read,1,{"Ewan","Silver",100,male}} = fly:read(State1, Type, {"Ewan","","",""}),
-	{read,1,{"Ewan","Silver",100,male}} = fly:read(State1, Type, {"","",100,""}),
-	{read,1,{"Alison","Silver",100,female}} = fly:read(State1, Type, {"","","",female}),
-	
-	{read_many,2,ReadList} = fly:read_many(State1,Type, {"","",100,""},10),	
-	{take_many,2,TakeList} = fly:take_many(State1,Type, {"","",100,""},10),
+check_can_write_and_then_take_string_test() ->
+	Typename = check_can_write_and_then_take_string_test,
+	{ok,_Pid} = fly:start(),
+	ok = fly:register(Typename,{string,string}),
+	{ok,10000} = fly:write(Typename,{"name","ewan"},10000),
+	{read,1,{"name","ewan"}} = fly:read(Typename,{"name",""}),
+	{take,1,{"name","ewan"}} = fly:take(Typename,{"name",""}),
+	{read,0,{}} = fly:read(Typename,{"name",""}),
+	shutdown_ok = fly:stop().
 
-	?assert(length(ReadList) == 2),
-	?assert(contains({"Ewan","Silver",100, male},ReadList)),
-	?assert(contains({"Alison","Silver",100, female},ReadList)),
+check_an_empty_take_test_dontrun() ->
+	Typename = check_an_empty_take_test,
+	{ok,_Pid} = fly:start(),
+	ok = fly:register(Typename,{string,string}),
+	{take,0,{}} = fly:take(Typename,{"not","defined"}),
+	shutdown_ok = fly:stop().
 
-	?assert(length(TakeList) == 2),
-	?assert(contains({"Ewan","Silver",100, male},TakeList)),
-	?assert(contains({"Alison","Silver",100, female},TakeList)),
-	
-	{read_many,0,[]} = fly:read_many(State1,Type, {"","",100,""},10),
-	{take_many,0,[]} = fly:take_many(State1,Type, {"","",100,""},10),
-	fly:close(State1).
+%%
+%% Local Functions
+%%
 
-check_can_mimic_a_blocking_read_test() ->
-	Type = check_can_mimic_a_blocking_read_test,
-	State = test_connect(),
-	{ok,State1} = fly:register_entry(State,{Type,{string,string,int,atom}}),
-		%% Check that we can mimic a blocking read and take 
-	Pause=500,
-	Now  = erlang:now(),
-	{read,0,{}} = fly:read(State1,Type, {"","",100,""},Pause),
-	true = Pause*1000 < timer:now_diff(erlang:now(),Now),
-	fly:close(State1).
-
-check_can_mimic_a_blocking_take_test() ->
-	Type = check_can_mimic_a_blocking_take_test,
-	State = test_connect(),
-	{ok,State1} = fly:register_entry(State,{Type,{string,string,int,atom}}),
-		%% Check that we can mimic a blocking read and take 
-	MaxTime = 500,
-	Pause={MaxTime,10},
-	Now  = erlang:now(),
-	{take,0,{}} = fly:take(State1,Type, {"","",100,""},Pause),
-	true = MaxTime*1000 < timer:now_diff(erlang:now(),Now),
-	fly:close(State1).
-
-check_an_empty_read_test() ->
-		%% Ensure that an empty read is properly handled.
-	Type = check_an_empty_read_test,
-	State = test_connect(),
-	{ok,State1} = fly:register_entry(State,{Type,{atom,tuple}}),
-	{read,0,{}} = fly:read(State1, Type, {test,""}),
-	fly:close(State1).
-
-check_an_empty_take_test() ->
-		%% Ensure that an empty take is properly handled.
-	Type = check_an_empty_take_test,
-	State = test_connect(),
-	{ok,State1} = fly:register_entry(State,{Type,{atom,tuple}}),
-	{take,0,{}} = fly:take(State1, Type, {test,""}),
-	fly:close(State1).
-
-check_we_can_use_a_generic_tuple_test() ->
-		%% Check we can send in a generic tuple
-	Type = generic_tuple,
-	State = test_connect(),
-	{ok,State1} = fly:register_entry(State,{Type,{atom,tuple}}),
-	{write,1000} = fly:write(State1, Type,{test,{a,tuple,"of",[1,2,4,"things"]}},1000),
-	{read,1,{test,{a,tuple,"of",[1,2,4,"things"]}}} = fly:read(State1, Type, {test,""}),
-	{read,1,{test,{a,tuple,"of",[1,2,4,"things"]}}} = fly:read(State1, Type, {"",{a,tuple,"of",[1,2,4,"things"]}}),
-	{take,1,{test,{a,tuple,"of",[1,2,4,"things"]}}} = fly:take(State1, Type, {test,""}),
-	fly:close(State1).
-
-check_we_can_use_a_generic_list_test() ->
-		%% Check we can send in a list tuple
-	Type = generic_list,
-	State = test_connect(),
-	{ok,State1} = fly:register_entry(State,{Type,{atom,list}}),
-	List = [1,2,3],
-	{write,1000} = fly:write(State1, Type,{test,List},1000),	
-	{read,1,{test,List}} = fly:read(State1, Type, {test,""}),
-	{read,1,{test,List}} = fly:read(State1, Type, {"",List}),
-	{take,1,{test,List}} = fly:take(State1, Type, {test,""}),
-	fly:close(State1).
-
-check_we_can_use_a_generic_binary_test() ->
-	Type = binary_read,
-	State = test_connect(),
-	{ok,State1} = fly:register_entry(State,{Type,{atom,binary}}),
-	Binary = term_to_binary(arandomterm),
-	{read,0,{}} = fly:read(State1, Type, {binarytest,""}),
-	{write,1000} = fly:write(State1, Type,{binarytest,Binary},1000),	
-	{read,1,{binarytest,Binary}} = fly:read(State1, Type, {binarytest,""}),
-	{read,1,{binarytest,Binary}} = fly:read(State1, Type, {"",Binary}),
-	{take,1,{binarytest,Binary}} = fly:take(State1, Type, {binarytest,""}),
-	{read,0,{}} = fly:read(State1, Type, {binarytest,""}),
-	fly:close(State1).
-
-	
-check_that_trying_to_register_a_type_of_the_same_name_fails_test() ->
-	State = test_connect(),
-	{ok,State1} = fly:register_entry(State,{type,{atom,tuple}}),
-	{fail, existing_type} = fly:register_entry(State1,{type,{tuple,atom}}),
-	%% Can we still read and write the original type?
-%%{write,1000} = write(State3, Atom,{"Ewan","Silver",100, male},1000),	
-%%	{read,1,{"Ewan","Silver",100,male}} = read(State3, Atom, {"Ewan","","",""}, 1000),
-%%	{take_many,1,[_]} = take_many(State3,Atom, {"","",100,""},10),
-	fly:close(State1).
-
-big_test() ->
-	State = test_connect(),
-%% The fact we have to call type_structrure_preamble is a hack at the moment. Need to find someway
-%% to hide this inside the returned State reference or something so that we can start to actually
-%% return the right kind of tuples.
-	Person = person,
-	{ok,State1} = fly:register_entry(State,Person,[{string,"name"},{string,"surname"},{int,"age"}, {atom,"sex"}]),
-
-	{write,1000} = fly:write(State1,Person,{"Monty","Silver",2, male},1000),
-	{write,1001} = fly:write(State1,Person,{"Elliot","Silver",6, male},1001),
-
-	{read_many,2,[_,_]} = fly:read_many(State1,Person, {"","Silver","",""},10),	
-
-	{read,1,{"Monty","Silver",2,male}} = fly:read(State1,Person, {"Monty","","",""}),
-	{read,1,{"Elliot","Silver",6,male}} = fly:read(State1,Person, {"","",6,""}),
-
-	{take,1,{"Monty","Silver",2,male}} = fly:take(State1, Person, {"Monty","",""}),
-	{take,1,{"Elliot","Silver",6,male}} = fly:take(State1, Person, {"","",6,""}),
-
-%% Check that the space is now empty again. Use all available checking calls.	
-	{take,0,{}} = fly:take(State1,Person,{"","Silver","",""}),
-	{read,0,{}} = fly:read(State1,Person,{"","Silver","",""}),
-	{read_many,0,[]} = fly:read_many(State1,Person, {"","Silver","",""},10),	
-	
-%% Finally we want to check that we can take multiple entries in one go.
-	{write,1000} = fly:write(State1, Person,{"Monty","Silver",2,""},1000),
-	{write,1001} = fly:write(State1, Person,{"Elliot","Silver",6,""},1001),
-	{read_many,2,[_,_]} = fly:read_many(State1, Person, {"","Silver","",""},10),	
-	{take_many,2,[_,_]} = fly:take_many(State1, Person, {"","Silver","",""},10),	
-	{take,0,{}} = fly:take(State1, Person,{"","Silver","",""}),
-	{read,0,{}} = fly:read(State1, Person,{"","Silver","",""}),
-	{read_many,0,[]} = fly:read_many(State1, Person, {"","Silver","",""},10),
-	{take_many,0,[]} = fly:take_many(State1, Person, {"","Silver","",""},10),	
-	
-%%	Now_multi  = erlang:now(),
-%%	{read_many,0,[]} = read_many(State2,Atom, {"","",100,""},Pause),	
-%%	true = Pause*1000 < timer:now_diff(erlang:now(),Now_multi),
-
-	ok = fly:close(State1).
-
-atom_test() ->
-	State = test_connect(),
-%% The fact we have to call type_structrure_preamble is a hack at the moment. Need to find someway
-%% to hide this inside the returned State reference or something so that we can start to actually
-%% return the right kind of tuples.
-	Person = atom_person,
-	{ok,State1} = fly:register_entry(State,Person,[{string,"name"},{atom,"sex"}]),
-	{write,1000} = fly:write(State1,Person,{"Monty", male},1000),
-	{read,1,{"Monty",male}} = fly:read(State1,Person, {"Monty",""}),
-	fly:close(State1).
-
-contains(Elem,List) ->
-	case (length(List) - length(lists:delete(Elem,List))) of
-		1 -> true;
-		_ -> false
-	end.
